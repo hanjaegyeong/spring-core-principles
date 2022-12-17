6d82df6cc44e6c9d6f23d6a3f3383aca54d7cfd8
hanjaegyeong <hjg000223@naver.com>
2022-12-17 21:23:10 +0900

    feat: config클래스 역할과 구현 분리

--- core/src/main/java/hello/core/Appconfig.java
+++ core/src/main/java/hello/core/Appconfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,20 @@ import hello.core.order.OrderServiceImpl;
 //config클래스는 생성한 객체 인스턴스의 참조(래퍼런스)를 생성자를 통해 주입해준다
 public class Appconfig {
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); //객체 인스턴스 생성을 통해 MemberService에서 사용할 구현체의 참조값을 넘김: 의존성 주입
+    //역할과 구현 분리
+    public MemberService memberService() { //역할
+        return new MemberServiceImpl(memberRepository()); //여기서 레포지토리 변경 안해도 됨
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private static MemberRepository memberRepository() { //구현
+        return new MemoryMemberRepository(); //이제 레포지토리 변경 시 이 부분만 변경하면 됨
+    }
+
+    public OrderService orderService() { //역할
+        return new OrderServiceImpl(memberRepository(), discountPolicy()); //여기서 레포지토리 변경 안해도 됨
+    }
+
+    public DiscountPolicy discountPolicy() { //구현
+        return new FixDiscountPolicy();
     }
 }
