fd657cfc11512279a99a0fb5a0d0758d2b1b4e27
hanjaegyeong <hjg000223@naver.com>
2022-12-22 23:23:56 +0900

    feat: 컴포넌트 스캔_탐색위치

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -4,11 +4,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-// excludeFilters: 컴포넌트 스캔 제외대상 지정, 기존에 만든 config클래스들 제외 : @Configuration도 내부에 @ComponentScan이 있어서 컴포넌트 스캔되니 제외
-// 컴포넌트 스캔은 @ComponentScan이 붙은 클래스를 스캔해 스프링 빈으로 자동 등록해줌
-// 사용하는 구현체들에 @Component 붙여줌 (serviceimpl, orderImpl, ratediscount, memoryrepository)
-// config파일에서 의존관계 주입 안해주니 구현체들 내부에서 @Autowired
+//스프링 부트로 생성 시 CoreApplication파일(루트폴더) 내부 애노테이션이 @ComponentScan 갖고 있으므로 따로 만들어줄 필요 없음
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        basePackages = "hello.core.member", //탐색할 시작 위치 지정(전체파일 탐색 안하고 해당 파일의 하위패키지만 탐색)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) //탐색 제외대상 지정
 public class AutoAppConfig {
 }
