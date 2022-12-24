405b704db40473793398bd360f379438c0d89892
hanjaegyeong <hjg000223@naver.com>
2022-12-24 13:14:40 +0900

    feat:프로토타입 스코프&싱글톤빈 함께 사용_provider 이용해서 해결

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,9 +4,14 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+
+
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,6 +19,7 @@ public class SingletonWithPrototypeTest1 {
 
     @Test
     void singletonClientUsePrototype() {
+
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class); //둘 다 받음
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
@@ -22,18 +28,17 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1); //두 번 호출했음에도 1, 1로 잘 리턴됨
     }
 
+    @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
-
+        //아래에서 ObjectProvider를 Provider로 바꾸면 Provider 사용 가능
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider; //provider 이용: 해당 빈만 찾아서 반환해주는 기능
 
-        public int logic() { //싱글톤 내부에서 prototypeBean.getCount() 호출
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); //get할 때마다 새로운 프로토타입 생성돼서 반환
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
