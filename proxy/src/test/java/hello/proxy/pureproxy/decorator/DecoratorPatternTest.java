9caa56cda3a1e21b6a25596b0ac37881f15aebce
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-18 22:11:30 +0900

    feat: 데코레이터 패턴 - 예제 코드1(적용전)

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,16 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+    @Test
+    void noDecorator() {
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new
+                DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+}
