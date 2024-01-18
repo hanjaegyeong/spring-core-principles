c2e06a2d7317fa49266d1a88901c065018f0ff58
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-18 22:07:45 +0900

    feat: 프록시패턴 - 예제 코드1

---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,16 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import hello.proxy.pureproxy.proxy.code.Subject;
+import org.junit.jupiter.api.Test;
+public class ProxyPatternTest {
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
