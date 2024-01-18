6cc3f06bc6f2bcdaf531e7c3d654661153359513
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-18 16:11:08 +0900

    feat: 쓰레드 로컬 동기화 - 적용

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,6 +2,7 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class LogTraceConfig {
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        //return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
