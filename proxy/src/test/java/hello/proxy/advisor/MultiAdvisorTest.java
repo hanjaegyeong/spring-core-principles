ae5e67bff4fca2d4c4f844f3af2098c90f34e03d
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-23 20:47:44 +0900

    feat: 예제 코드4 - 여러 어드바이저 함께 적용2

--- proxy/src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
+++ proxy/src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -51,4 +51,21 @@ public class MultiAdvisorTest {
             return invocation.proceed();
         }
     }
+
+    @Test
+    @DisplayName("하나의 프록시, 여러 어드바이저")
+    void multiAdvisorTest2() {
+        //proxy -> advisor2 -> advisor1 -> target
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        proxyFactory1.addAdvisor(advisor2);
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory1.getProxy();
+
+        //실행
+        proxy.save();
+    }
 }
