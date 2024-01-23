75feb87fba83ee8cdd1ece36568d96e606ed968f
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-23 20:37:38 +0900

    feat: 예제 코드1 - 어드바이저

---
+++ proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,30 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import hello.proxy.common.advice.TimeAdvice;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class AdvisorTest {
+    @Test
+    void advisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+}
