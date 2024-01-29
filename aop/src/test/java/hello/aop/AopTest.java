3606e9f78377852f8495951286f8e236f5a4c394
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-29 20:40:12 +0900

    feat: 스프링 AOP 구현1 - 시작

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,15 +2,19 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
+@Import(AspectV1.class) //추가
 @SpringBootTest
 public class AopTest {
 
