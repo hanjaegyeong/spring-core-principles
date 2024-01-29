58cb235bcb46b42278268b93a9797244dd5e144e
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-29 22:07:20 +0900

    feat: 프록시와 내부 호출 - 대안3 구조 변경

---
+++ aop/src/test/java/hello/aop/internalcall/CallServiceV3Test.java
@@ -0,0 +1,21 @@
+package hello.aop.internalcall;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.aop.internalcall.aop.CallLogAspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV3Test {
+    @Autowired
+    CallServiceV3 callServiceV3;
+
+    @Test
+    void external() {
+        callServiceV3.external();
+    }
+}
