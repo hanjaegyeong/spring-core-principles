5e3687bdd714d0b16c39c8f84c8d06e94845ef90
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-29 22:03:13 +0900

    feat: 프록시와 내부 호출 - 대안2 지연 조회

---
+++ aop/src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,29 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * ObjectProvider(Provider), ApplicationContext를 사용해서 지연(LAZY) 조회
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV2 {
+    // private final ApplicationContext applicationContext;
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public void external() {
+        log.info("call external");
+        // CallServiceV2 callServiceV2 =applicationContext.getBean(CallServiceV2.class);
+        CallServiceV2 callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal(); //외부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
